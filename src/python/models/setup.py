ea7ae70d1c6f8340bce55f3cc5b7fca597117e05
pletessier <pierre.letessier@gmail.com>
2019-08-19 18:03:04 +0200

    Add circle-ci

---
+++ src/python/models/setup.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+"""
+   Taranis Client
+"""
+
+from setuptools import setup  # noqa: H301
+
+NAME = "taranis-cli"
+VERSION = "0.1.0"
+REQUIRES = ["grpcio==1.21.1", "grpcio-tools==1.21.1"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Taranis client",
+    author="Pierre Letessier",
+    author_email="",
+    url="https://github.com/pletessier/taranis",
+    keywords=["lib", "taranis", "client"],
+    install_requires=REQUIRES,
+    packages=["."],
+    include_package_data=True,
+    long_description="""\
+    Taranis client
+    """
+)
